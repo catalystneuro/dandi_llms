eeefe1edffb65e1b6062e0a93a72d29d862e831a
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-07-18 12:50:40 +0200

    added metadata grounding

--- utils/metadata_extraction.py
+++ utils/metadata_extraction.py
@@ -1,9 +1,11 @@
 import json
 import requests
 from pathlib import Path
+import os
 
 import openai
-import os
+from langchain.embeddings.openai import OpenAIEmbeddings
+from qdrant_client import QdrantClient
 
 root_path = Path(__file__).parent.parent
 data_dir = root_path / "data"
@@ -21,14 +23,16 @@ def generate_prompt_from_dandiset_id(dandiset_id: str) -> str:
 
 
 def generate_prompt_for_example(abstract: str, expected_metadata: dict) -> str:
+    keys_to_extract = ["species_names", "anatomy", "approach_names", "measurement_names"]
+    metadata_to_extract = {k: expected_metadata[k] for k in keys_to_extract}
     prompt = f"""The abstract of the paper is:
     {abstract} 
-    Information: {json.dumps(expected_metadata)}
+    Information: {json.dumps(metadata_to_extract)}
     """
     return prompt
 
 
-def generate_zero_shot_prompt(dandiset_ids: list) -> str:
+def generate_prompt_examples(dandiset_ids: list) -> str:
     examples_prompt = ""
     for i, dandiset_id in enumerate(dandiset_ids):
         examples_prompt += (
@@ -70,7 +74,7 @@ def generate_task_prompt_from_abstract(abstract: str) -> str:
 
     Fill as in the examples:
     Information: {{}}
-    In the format of the previous reponse. If some information is missing, leave it blank.
+    In the format of the previous response. If some information is missing, leave it blank.
     """
     return prompt
 
@@ -92,6 +96,78 @@ def query_metadata(prompt):
     response = completion.choices[0].message.content
     return response
 
+
+def ground_metadata_in_ontologies(plain_metadata):
+    
+    embedding_model = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
+    url = "https://c1490259-dfe4-4a49-8712-24f690d450f6.us-east-1-0.aws.cloud.qdrant.io:6333"
+    api_key =os.environ["QDRANT_API_KEY"]
+    client = QdrantClient(
+    url=url,
+    api_key=api_key,
+    )
+    
+    grounded_meatdata = _ground_metadata_in_ontologies_uberon(client, embedding_model, plain_metadata)
+    grounded_metadat = _ground_metadata_in_ontologies_ncbi_taxon(client, embedding_model, plain_metadata)
+    
+    return grounded_meatdata
+
+
+def _ground_metadata_in_ontologies_uberon(client, embedding_model, plain_metadata):
+    collection_name = "uberon"
+    uberon_key_to_url = lambda x: f"http://purl.obolibrary.org/obo/{x.replace(':', '_')}"
+
+    anatomy_terms_list =  plain_metadata["anatomy"]
+    term_embedding_list = embedding_model.embed_documents(anatomy_terms_list)
+    anatomy_term_to_embedings = {anatomy_terms_list[i]: term_embedding_list[i] for i in range(len(anatomy_terms_list))}
+
+    top = 1  # The number of similar vectors you want to retrieve
+    term_to_identifiers_dict = {}
+    for anatomy_term, embedding in anatomy_term_to_embedings.items():
+        
+        query_vector = embedding
+        search_results = client.search(collection_name=collection_name, query_vector=query_vector, limit=top, with_payload=True, with_vectors=False)
+        
+        uberon_ids = [result.payload["uberon_id"] for result in search_results]
+        uberon_urls = [uberon_key_to_url(uberon_id) for uberon_id in uberon_ids]
+        
+        term_to_identifiers_dict[anatomy_term] = (uberon_ids[0], uberon_urls[0])
+        
+    
+    plain_metadata[f"anatomy_identifiers"] = [term_to_identifiers_dict[anatomy_term][0] for anatomy_term in anatomy_terms_list]
+    plain_metadata["anatomy_urls"] = [term_to_identifiers_dict[anatomy_term][1] for anatomy_term in anatomy_terms_list]
+    
+    return plain_metadata
+    
+
+def _ground_metadata_in_ontologies_ncbi_taxon(client, embedding_model, plain_metadata):
+    collection_name = "ncbi_taxon"
+    ncbi_key_to_url = lambda x: f"http://purl.obolibrary.org/obo/{x.replace(':', '_')}"
+
+    species_term_list =  plain_metadata["species_names"]
+    if species_term_list == []:
+        return plain_metadata
+    
+    term_embedding_list = embedding_model.embed_documents(species_term_list)
+    species_term_to_embedings = {species_term_list[i]: term_embedding_list[i] for i in range(len(species_term_list))}
+
+    top = 1  # The number of similar vectors you want to retrieve
+    term_to_identifiers_dict = {}
+    for species_term, embedding in species_term_to_embedings.items():
+        
+        query_vector = embedding
+        search_results = client.search(collection_name=collection_name, query_vector=query_vector, limit=top, with_payload=True, with_vectors=False)
+        
+        uberon_ids = [result.payload["ncbi_taxon_id"] for result in search_results]
+        uberon_urls = [ncbi_key_to_url(uberon_id) for uberon_id in uberon_ids]
+        
+        term_to_identifiers_dict[species_term] = (uberon_ids[0], uberon_urls[0])
+        
+    
+    plain_metadata[f"species_identifiers"] = [term_to_identifiers_dict[species_term][0] for species_term in species_term_list]
+    plain_metadata["species_urls"] = [term_to_identifiers_dict[species_term][1] for species_term in species_term_list]
+    
+    return plain_metadata
 
 def parse_response_to_dict(response):
     start_index = response.find("{")
